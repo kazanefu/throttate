da94d5e2507be65491eea94a636c903a4a39eead
kazanefu <kazanefu12@gmail.com>
2026-05-05 15:35:25 +0900

    second refactor

--- src/playing/player/collision/checkpoint.rs
+++ src/playing/player/collision/checkpoint.rs
@@ -1,11 +1,8 @@
-use crate::{
-    action_effect::FireCheckPointEffect,
-    course::course_items::checkpoint::CheckPoint,
-};
-use bevy::prelude::*;
-use bevy_rapier2d::prelude::*;
 use super::get_player_collision;
 use crate::playing::player::{Player, TargetCheckPoint};
+use crate::{action_effect::FireCheckPointEffect, course::course_items::checkpoint::CheckPoint};
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 pub fn reach_checkpoint(
     mut player_query: Query<(Entity, &mut TargetCheckPoint), With<Player>>,
@@ -20,15 +17,16 @@ pub fn reach_checkpoint(
     for event in collision_event.read() {
         if let CollisionEvent::Started(e1, e2, _) = *event
             && let Some(other) = get_player_collision(e1, e2, player_entity)
-                && let Ok((checkpoint, transform)) = checkpoint_query.get(other)
-                    && checkpoint.priority() >= target_checkpoint.priority {
-                        let prev_position = target_checkpoint.position;
-                        target_checkpoint.priority = checkpoint.priority();
-                        target_checkpoint.position = transform.translation;
-                        
-                        if prev_position != target_checkpoint.position {
-                            checkpoint_effect_writer.write(FireCheckPointEffect(target_checkpoint.position));
-                        }
-                    }
+            && let Ok((checkpoint, transform)) = checkpoint_query.get(other)
+            && checkpoint.priority() >= target_checkpoint.priority
+        {
+            let prev_position = target_checkpoint.position;
+            target_checkpoint.priority = checkpoint.priority();
+            target_checkpoint.position = transform.translation;
+
+            if prev_position != target_checkpoint.position {
+                checkpoint_effect_writer.write(FireCheckPointEffect(target_checkpoint.position));
+            }
+        }
     }
 }
