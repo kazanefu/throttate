c6b8f0e9730d9cb0e08a7869aba4801066ce571b
kazanefu <kazanefu12@gmail.com>
2026-05-13 11:20:33 +0900

    fix text box font

--- src/course/spawn.rs
+++ src/course/spawn.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::JpFont;
+
 use super::course_items::*;
 
 use super::*;
@@ -11,7 +13,7 @@ pub fn spawn_course_from_id(
     mut commands: Commands,
     course_list_res: Res<CourseListResource>,
     mut spawn_course_message: MessageReader<SpawnCourseMessage>,
-    asset_server: Res<AssetServer>,
+    font: Res<JpFont>,
 ) {
     for SpawnCourseMessage(id) in spawn_course_message.read() {
         let course = course_list_res
@@ -20,27 +22,43 @@ pub fn spawn_course_from_id(
             .find(|(course_entry, _course)| course_entry.id == *id);
         match course {
             Some((_course_entry, course)) => {
-                let course_entity = commands.spawn((CourseID::new(*id),Transform::from_xyz(0.0, 0.0, 0.0))).id();
+                let course_entity = commands
+                    .spawn((CourseID::new(*id), Transform::from_xyz(0.0, 0.0, 0.0)))
+                    .id();
                 for entity in &course.entities {
-                    let item_entity = spawn_course_from_entities(&mut commands, entity, &asset_server).id();
+                    let item_entity =
+                        spawn_course_from_entities(&mut commands, entity, font.get()).id();
                     commands.entity(course_entity).add_child(item_entity);
                 }
-                
-            },
+            }
             None => warn!("failed to get course from course list loaded at the start of game"),
         }
     }
 }
 
-fn spawn_course_from_entities<'a>(commands: &'a mut Commands, entity: &EntityData, asset_server: &AssetServer)-> EntityCommands<'a> {
-    let (x,y) = (entity.x,entity.y);
+fn spawn_course_from_entities<'a>(
+    commands: &'a mut Commands,
+    entity: &EntityData,
+    font: &Handle<Font>,
+) -> EntityCommands<'a> {
+    let (x, y) = (entity.x, entity.y);
     match &entity.kind {
-        EntityKind::Ground { width, height } => commands.spawn(ground::ground_bundle(x, y, *width, *height)),
-        EntityKind::Checkpoint { priority } => commands.spawn(checkpoint::check_point_bundle(x, y, *priority)),
-        EntityKind::Breakable { required_speed } => commands.spawn(breakable_box::breakable_box_bundle(x, y, *required_speed)),
+        EntityKind::Ground { width, height } => {
+            commands.spawn(ground::ground_bundle(x, y, *width, *height))
+        }
+        EntityKind::Checkpoint { priority } => {
+            commands.spawn(checkpoint::check_point_bundle(x, y, *priority))
+        }
+        EntityKind::Breakable { required_speed } => {
+            commands.spawn(breakable_box::breakable_box_bundle(x, y, *required_speed))
+        }
         EntityKind::Death => commands.spawn(death_box::death_box_bundle(x, y)),
-        EntityKind::Turret { interval ,rotation} => commands.spawn(turret::turret_bundle(x, y, *interval, *rotation)),   
+        EntityKind::Turret { interval, rotation } => {
+            commands.spawn(turret::turret_bundle(x, y, *interval, *rotation))
+        }
         EntityKind::Goal => commands.spawn(goal::goal_bundle(x, y)),
-        EntityKind::Text { sentence } => commands.spawn(text_box::text_box_bundle(x, y, sentence, asset_server)),
+        EntityKind::Text { sentence } => {
+            commands.spawn(text_box::text_box_bundle(x, y, sentence, font))
+        }
     }
 }
